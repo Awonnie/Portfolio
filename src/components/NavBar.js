92c44f0f9bbf8222977d4e4b04a768a65c199657
Awonnie <97012983+Awonnie@users.noreply.github.com>
2024-04-19 03:09:32 +0800

    finish NavBAr

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,53 @@
-import Link from "next/link"
-import Logo from "./Logo"
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { GithubIcon, LinkedInIcon } from "./Icons";
+import Logo from "./Logo";
+
+
+const CustomLink = ({href, title, className=""}) => {
+  const router = useRouter();
+  console.log(router)
+
+  return(
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 
+      group-hover:w-full transition-[width] ease duration-300
+      ${router.asPath === href ? 'w-full' : 'w-0'}`}>
+        &nbsp;
+      </span>
+    </Link>
+  )
+}
 
 const NavBar = () => {
   return (
-    <header className='w-full px-32 py-8 front-medium flex items-center justify-betweem'>
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/articles">Articles</Link>
+            <CustomLink href="/" title="Home" className="mr-4"/>
+            <CustomLink href="/about" title="About" className="mx-4"/>
+            <CustomLink href="/projects" title="Projects" className="mx-4"/>
+            <CustomLink href="/articles" title="Articles" className="ml-4"/>
         </nav>
+        <nav className ="flex items-center justify-center flex-wrap">
+            <motion.a href="https://github.com/Awonnie" target={"_blank"}
+            className="w-10 mr-5"
+            whileHover={{y:-2}}
+            whileTap={{scale:0.9}}>
+              <GithubIcon/>
+            </motion.a>
+
+            <motion.a href="https://www.linkedin.com/in/angie-wong-mei-chi/" target={"_blank"}
+            className="w-10 mr-5"
+            whileHover={{y:-2}}
+            whileTap={{scale:0.9}}>
+              <LinkedInIcon/>
+            </motion.a>
+        </nav>
+        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo/>
-        <nav>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-        </nav>
+        </div>
     </header>
   )
 }
